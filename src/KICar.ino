7fd68077be29e5320ebb6e004c22f54cd5ebd199
HannHank <hanke.johann@gmail.com>
2019-03-17 12:11:39 +0100

    Added some headerfiles
    I should have done ist sooner, this is just a
    small stand between, it can still be sorted some
    parts better.

--- src/KICar.ino
+++ src/KICar.ino
@@ -2,22 +2,23 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h> 
-#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <SparkFun_MAG3110.h>
 #include <Adafruit_GFX.h> 
-#include <MCUFRIEND_kbv.h>
+// #include <MCUFRIEND_kbv.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
+#include <GPS.h>
+#include <CompassDraw.h>
 //end
 MAG3110 mag = MAG3110();
 //const for compass Draw_Compass_Rose
-const int centreX = 160;
-const int centreY = 120;
-const int diameter = 100;
-const int centreX1 = 160;
-const int centreY1 = 120;
-const int diameter1 = 100;
+// const int centreX = 160;
+// const int centreY = 120;
+// const int diameter = 100;
+// const int centreX1 = 160;
+// const int centreY1 = 120;
+// const int diameter1 = 100;
 //end
 //defin the steering thinks
 #define TURN_LEFT 1
@@ -39,47 +40,46 @@ long olddistance = 0;
 
 
 ////GPS stuff
-static const int RXPin = 51, TXPin = 53;
-static const uint32_t GPSBaud = 9600;
-double LONDON_LAT, LONDON_LON;
+// static const int RXPin = 51, TXPin = 53;
+// static const uint32_t GPSBaud = 9600;
+// double LONDON_LAT, LONDON_LON;
 // The TinyGPS++ object
-TinyGPSPlus gps;
-SoftwareSerial ss(RXPin, TXPin);
+
 /////////////////////////////////////////////////////////////////
-MCUFRIEND_kbv tft;
+// MCUFRIEND_kbv tft;
 
-// The control pins for the LCD can be assigned to any digital or
-// analog pins...but we'll use the analog pins as this allows us to
-// double
-//up the pins with the touch screen (see the TFT paint example).
-#define DHTPIN 51
-#define DHTTYPE DHT11
+// // The control pins for the LCD can be assigned to any digital or
+// // analog pins...but we'll use the analog pins as this allows us to
+// // double
+// //up the pins with the touch screen (see the TFT paint example).
+// #define DHTPIN 51
+// #define DHTTYPE DHT11
 
-#define BLACK 0x0000
-#define BLUE 0x001F
-#define RED 0xF800
-#define GREEN 0x07E0
-#define CYAN 0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW 0xFFE0
-#define WHITE 0xFFFF
-// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
-// For the Arduino Uno, Duemilanove, Diecimila, etc.:
-//   D0 connects to digital pin 8  (Notice these are
-//   D1 connects to digital pin 9   NOT in order!)
-//   D2 connects to digital pin 2
-//   D3 connects to digital pin 3
-//   D4 connects to digital pin 4
-//   D5 connects to digital pin 5
-//   D6 connects to digital pin 6
-//   D7 connects to digital pin 7
-//   Assign human-readable names to some common 16-bit colour values:
+// #define BLACK 0x0000
+// #define BLUE 0x001F
+// #define RED 0xF800
+// #define GREEN 0x07E0
+// #define CYAN 0x07FF
+// #define MAGENTA 0xF81F
+// #define YELLOW 0xFFE0
+// #define WHITE 0xFFFF
+// // When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
+// // For the Arduino Uno, Duemilanove, Diecimila, etc.:
+// //   D0 connects to digital pin 8  (Notice these are
+// //   D1 connects to digital pin 9   NOT in order!)
+// //   D2 connects to digital pin 2
+// //   D3 connects to digital pin 3
+// //   D4 connects to digital pin 4
+// //   D5 connects to digital pin 5
+// //   D6 connects to digital pin 6
+// //   D7 connects to digital pin 7
+// //   Assign human-readable names to some common 16-bit colour values:
 
-const int x_offset = 30;
-const int y_offset = 128;
-const int z_offset = 0;
-int last_dx, last_dy, dx, dy;
-int last_dx1, last_dy1, dx1, dy1;
+// const int x_offset = 30;
+// const int y_offset = 128;
+// const int z_offset = 0;
+// int last_dx, last_dy, dx, dy;
+// int last_dx1, last_dy1, dx1, dy1;
 int status = 0;
 
 // Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
@@ -99,10 +99,11 @@ void setup()
   Wire.begin(); // for the Compass
   mag.initialize(); // start Compass
   // Put the HMC5883 IC into the correct operating mode
-  last_dx = centreX;
-  last_dy = centreY;
-  last_dx1 = centreX1;
-  last_dy1 = centreY1;
+  // last_dx = centreX;
+  // last_dy = centreY;
+  // last_dx1 = centreX1;
+  // last_dy1 = centreY1;
+  DrawCompassSetup();
   ////////////////////////////////////////////////////////////////
   ss.begin(GPSBaud); //gps start's
   ///////////////////////////////////////////////////////////////
@@ -289,19 +290,10 @@ mag.readMag(&x, &y, &z);
   delay(25);
 
   //printing GPS stuff
-  printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
-  printInt(gps.hdop.value(), gps.hdop.isValid(), 5);
-  printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
-  printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
-  printInt(gps.location.age(), gps.location.isValid(), 5);
-  printDateTime(gps.date, gps.time);
-  printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
-  printFloat(gps.course.deg(), gps.course.isValid(), 7, 2);
-  printFloat(gps.speed.kmph(), gps.speed.isValid(), 6, 2);
-  printStr(gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.value()) : "*** ", 6);
+  gps_log();
  //end 
  //calculate the distance to the destination 
-  //unsigned long distanceKmToLondon = *have to look why I wrote this line XD
+  unsigned long distanceKmToLondon = //*have to look why I wrote this line XD
     (unsigned long)TinyGPSPlus::distanceBetween(
       gps.location.lat(),
       gps.location.lng(),
@@ -353,54 +345,54 @@ double getBearing(double lat1, double lng1, double lat2, double lng2)
   return fmod((degrees(atan2(dLng, dPhi)) + 360.0), 360.0);
 }
 //do GPS and tft stuff
-void display_item(int x, int y, String token, int txt_colour, int txt_size)
-{
-  tft.setCursor(x, y);
-  tft.setTextColor(txt_colour);
-  tft.setTextSize(txt_size);
-  tft.print(token);
-  tft.setTextSize(2); // Back to default text size
-}
+// void display_item(int x, int y, String token, int txt_colour, int txt_size)
+// {
+//   tft.setCursor(x, y);
+//   tft.setTextColor(txt_colour);
+//   tft.setTextSize(txt_size);
+//   tft.print(token);
+//   tft.setTextSize(2); // Back to default text size
+// }
 
-void arrow(int x2, int y2, int x1, int y1, int alength, int awidth, int colour)
-{
-  float distance;
-  int dx, dy, x2o, y2o, x3, y3, x4, y4, k;
-  distance = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
-  dx = x2 + (x1 - x2) * alength / distance;
-  dy = y2 + (y1 - y2) * alength / distance;
-  k = awidth / alength;
-  x2o = x2 - dx;
-  y2o = dy - y2;
-  x3 = y2o * k + dx;
-  y3 = x2o * k + dy;
-  //
-  x4 = dx - y2o * k;
-  y4 = dy - x2o * k;
-  tft.drawLine(x1, y1, x2, y2, colour);
-  tft.drawLine(x1, y1, dx, dy, colour);
-  tft.drawLine(x3, y3, x4, y4, colour);
-  tft.drawLine(x3, y3, x2, y2, colour);
-  tft.drawLine(x2, y2, x4, y4, colour);
-}
+// void arrow(int x2, int y2, int x1, int y1, int alength, int awidth, int colour)
+// {
+//   float distance;
+//   int dx, dy, x2o, y2o, x3, y3, x4, y4, k;
+//   distance = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+//   dx = x2 + (x1 - x2) * alength / distance;
+//   dy = y2 + (y1 - y2) * alength / distance;
+//   k = awidth / alength;
+//   x2o = x2 - dx;
+//   y2o = dy - y2;
+//   x3 = y2o * k + dx;
+//   y3 = x2o * k + dy;
+//   //
+//   x4 = dx - y2o * k;
+//   y4 = dy - x2o * k;
+//   tft.drawLine(x1, y1, x2, y2, colour);
+//   tft.drawLine(x1, y1, dx, dy, colour);
+//   tft.drawLine(x3, y3, x4, y4, colour);
+//   tft.drawLine(x3, y3, x2, y2, colour);
+//   tft.drawLine(x2, y2, x4, y4, colour);
+// }
 
-void Draw_Compass_Rose()
-{
-  int dxo, dyo, dxi, dyi;
-  tft.drawCircle(centreX, centreY, diameter, YELLOW); // Draw compass circle
-  for (float i = 0; i < 360; i = i + 22.5)
-  {
-    dxo = diameter * cos((i - 90) * 3.14 / 180);
-    dyo = diameter * sin((i - 90) * 3.14 / 180);
-    dxi = dxo * 0.9;
-    dyi = dyo * 0.9;
-    tft.drawLine(dxi + centreX, dyi + centreY, dxo + centreX, dyo + centreY, YELLOW);
-  }
-  display_item((centreX - 5), (centreY - 85), "N", RED, 2);
-  display_item((centreX - 5), (centreY + 70), "S", RED, 2);
-  display_item((centreX + 80), (centreY - 5), "E", RED, 2);
-  display_item((centreX - 85), (centreY - 5), "W", RED, 2);
-}
+// void Draw_Compass_Rose()
+// {
+//   int dxo, dyo, dxi, dyi;
+//   tft.drawCircle(centreX, centreY, diameter, YELLOW); // Draw compass circle
+//   for (float i = 0; i < 360; i = i + 22.5)
+//   {
+//     dxo = diameter * cos((i - 90) * 3.14 / 180);
+//     dyo = diameter * sin((i - 90) * 3.14 / 180);
+//     dxi = dxo * 0.9;
+//     dyi = dyo * 0.9;
+//     tft.drawLine(dxi + centreX, dyi + centreY, dxo + centreX, dyo + centreY, YELLOW);
+//   }
+//   display_item((centreX - 5), (centreY - 85), "N", RED, 2);
+//   display_item((centreX - 5), (centreY + 70), "S", RED, 2);
+//   display_item((centreX + 80), (centreY - 5), "E", RED, 2);
+//   display_item((centreX - 85), (centreY - 5), "W", RED, 2);
+// }
 void GPS()
 {
   tft.setCursor(0, 0);
@@ -433,82 +425,5 @@ void GPS()
   //tft.setCursor(0,300);
   //tft.print(gps.location.lng());
   // printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
-}
-static void printInt(unsigned long val, bool valid, int len)
-{
-  char sz[32] = "*****************";
-  if (valid)
-    sprintf(sz, "%ld", val);
-  sz[len] = 0;
-  for (int i = strlen(sz); i < len; ++i)
-    sz[i] = ' ';
-  if (len > 0)
-    sz[len - 1] = ' ';
-  Serial.print(sz);
-}
-static void smartDelay(unsigned long ms)
-{
-  unsigned long start = millis();
-  do
-  {
-    while (ss.available())
-      gps.encode(ss.read());
-  } while (millis() - start < ms);
-}
-
-static void printFloat(float val, bool valid, int len, int prec)
-{
-  if (!valid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
-  }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i = flen; i < len; ++i)
-      Serial.print(' ');
-  }
-  smartDelay(0);
-}
-
-static void printDateTime(TinyGPSDate & d, TinyGPSTime & t)
-{
-  if (!d.isValid())
-  {
-    Serial.print(F("********** "));
-  }
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
-    Serial.print(sz);
-  }
-
-  if (!t.isValid())
-  {
-    Serial.print(F("******** "));
-  }
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
-    Serial.print(sz);
-  }
-
-  printInt(d.age(), d.isValid(), 5);
-  smartDelay(0);
-}
-
-static void printStr(const char *str, int len)
-{
-  int slen = strlen(str);
-  for (int i = 0; i < len; ++i)
-    Serial.print(i < slen ? str[i] : ' ');
-  smartDelay(0);
 }
 
